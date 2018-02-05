f169437087f201542b7fd49cc73bd7bf15f51ef5
Paramonov Dmitriy Valerevich <forestplus@ya.ru>
2018-02-06 00:11:27 +0300

    NEW commit

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
   <title>TEST PHP</title>
  </head>
  <body>
- <?php echo '<p>Hello, DevOps!</p>'; ?>
- <?php echo '<p>Hello, DevOps!</p>'; ?>
- </body>
+ <?php echo '<p>Hello, DevOps! NEW</p>'; ?>
+  </body>
 </html>
